f18121fd29d14d0169a365616991b70c3666354d
Muniox <muniox20@gmail.com>
2023-08-23 15:11:56 +0200

    Added revision in nest10 for d16

---
+++ d18_nestjs_pipes_nest10/src/basket/basket.module.ts
@@ -0,0 +1,13 @@
+import { Module, forwardRef } from '@nestjs/common';
+import { BasketController } from './basket.controller';
+import { BasketService } from './basket.service';
+import { ShopModule } from '../shop/shop.module';
+import { UserModule } from '../user/user.module';
+
+@Module({
+  imports: [forwardRef(() => ShopModule), forwardRef(() => UserModule)],
+  controllers: [BasketController],
+  providers: [BasketService],
+  exports: [BasketService],
+})
+export class BasketModule {}
