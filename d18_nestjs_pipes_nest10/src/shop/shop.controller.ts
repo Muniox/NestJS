c51f467b70f88f454eec85f458d9b8069c8608b0
Muniox <muniox20@gmail.com>
2023-08-24 15:49:19 +0200

    Added revision in nest10 for d18

--- d18_nestjs_pipes_nest10/src/shop/shop.controller.ts
+++ d18_nestjs_pipes_nest10/src/shop/shop.controller.ts
@@ -1,6 +1,15 @@
-import { Controller, Get, Inject } from '@nestjs/common';
+import {
+  Controller,
+  DefaultValuePipe,
+  Get,
+  HttpStatus,
+  Inject,
+  Param,
+  ParseIntPipe,
+} from '@nestjs/common';
 import { ShopService } from './shop.service';
 import { ShopItemInterface } from 'src/interfaces/shop';
+import { CheckAgePipe } from 'src/pipes/check-age';
 
 @Controller('shop')
 export class ShopController {
@@ -9,5 +18,27 @@ export class ShopController {
   @Get('/')
   async getShopList(): Promise<ShopItemInterface[]> {
     return await this.shopService.getItems();
+  }
+
+  //jeśli wyślemy w parametrze wyraz to dostaniemy błąd bo nam waliduje i oczekuje liczby
+  @Get('/test/:index?')
+  test(
+    @Param(
+      'index',
+      new DefaultValuePipe(0),
+      new ParseIntPipe({
+        errorHttpStatusCode: HttpStatus.FORBIDDEN,
+      }),
+    )
+    index?: string,
+  ) {
+    console.log(typeof index);
+    return '';
+  }
+
+  @Get('/test/:age')
+  checkAge(@Param('age', new CheckAgePipe({ minAge: 21 })) age: number) {
+    console.log(typeof age, age);
+    return '';
   }
 }
