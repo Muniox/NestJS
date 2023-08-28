39cd7c3b7a6bc1bb7ed167247eee7f9463037852
Muniox <muniox20@gmail.com>
2023-08-28 21:37:59 +0200

    Added revision for d21 in nest10

--- d21a_nestjs_express_module_cooperation_with_nest10(helmet)/src/main.ts
+++ d21a_nestjs_express_module_cooperation_with_nest10(helmet)/src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { GlobalExceptionFilter } from './filters/global-exception.filter';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import helmet from 'helmet';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useGlobalFilters(new GlobalExceptionFilter());
+  app.use(helmet());
 
   await app.listen(3000);
 }
